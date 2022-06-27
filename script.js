3b3cf3925dda1c65f6a841073be93d1d2ed4dda1
Maralena86 <mari16cobain@msn.com>
2022-06-27 22:46:01 +0200

    change colors

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ let canvas = document.querySelector('canvas');
 		context.fillRect(100, 100, 400, 400);
 		
 		context.lineWidth = 5;
-		context.lineWidth
 		context.beginPath();
 		context.rect(100, 100, 400, 400);
 		//context.stroke();
@@ -14,29 +13,31 @@ let canvas = document.querySelector('canvas');
 		context.arc(300, 300, 100, 0, Math.PI * 2);
 		
 		
-		const width = 50;
-		const height = 50;
-		const gap = 30;
-		let x, y;
+		
 
 		for (let i = 0; i < 5; i++) 
 		{
+			const width = 50;
+			const height = 50;
+			const gap = 30;
+			let x, y;
 			for (let j = 0; j < 5; j++) {
 							
 				x = 50 + (width + gap) *i;
-				y = 50 + (width + gap) *j;			
+				y = 50 + (width + gap) *j;
+				
+				
 			context.beginPath();
-		
-		
 			context.rect(x,y, width, height);
+			context.strokeStyle="red";
 			context.stroke();
-			context.strokeStyle="white";
 	
 			
-			if (Math.random() > 0.5) 
+			if (Math.random() > 0.7) 
 			{
 				context.beginPath();
 				context.rect(x + 9, y + 9, width -18, height -18)
+				context.strokeStyle="green";
 				context.stroke();
 				
 
